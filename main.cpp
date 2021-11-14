d185a3b082f5b003d497ffb6191be2c6fa05f9b7
amodioandrea <92790243+amodioandrea@users.noreply.github.com>
2021-11-14 22:42:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n;
+    cin >> n;
+
+        int x = 1, y = 1, z;
+    if (n>3){
+        for (int i=0; i <n; i++) {
+            z = x + y;
+            cout << x << endl;
+            if (x <= n) {
+                x = y;
+                y = z;
+            }
+        }
+                }else{
+                    for(int i=-1;i<n;i++){
+                        z = x + y;
+                        cout << x << endl;
+                        if (x <= n) {
+                            x = y;
+                            y = z;}
+                    }
+                }
+    return 0;
 }
